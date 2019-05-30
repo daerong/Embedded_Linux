d955465e5ec086967874b39868c37a423d31532f
Daeseong Yu <yu5501@nate.com>
2019-05-30 13:30:49 +0900

    숫자야구 게임, fpga 테스트 소스 연결 전

---
+++ Linux/game/fpga_game_test.c
@@ -0,0 +1,271 @@
+#include "../include/fpga_test.h"
+
+#include <termios.h>
+#include <stdio.h>
+
+
+
+#define ENTER 13
+#define ESCAPE 27
+#define BACK_SPACE 8
+
+#define ZERO 48
+#define ONE 49
+#define TWO 50
+#define THREE 51
+#define FOUR 52
+#define FIVE 53
+#define SIX 54
+#define SEVEN 55
+#define EIGHT 56
+#define NINE 57
+
+#define KEY_ZERO 96
+#define KEY_ONE 97
+#define KEY_TWO 98
+#define KEY_THREE 99
+#define KEY_FOUR 100
+#define KEY_FIVE 101
+#define KEY_SIX 102
+#define KEY_SEVEN 103
+#define KEY_EIGHT 104
+#define KEY_NINE 105
+
+#define LEFT 37
+#define UP 38
+#define RIGHT 39
+#define DOWN 40
+
+
+
+static struct termios before, after;
+
+/* Initialize after terminal i/o settings */
+void initTermios(int echo)
+{
+	tcgetattr(0, &before); /* grab before terminal i/o settings */
+	after = before; /* make after settings same as before settings */
+	after.c_lflag &= ~ICANON; /* disable buffered i/o */
+	if (echo) {
+		after.c_lflag |= ECHO; /* set echo mode */
+	}
+	else {
+		after.c_lflag &= ~ECHO; /* set no echo mode */
+	}
+	tcsetattr(0, TCSANOW, &after); /* use these after terminal i/o settings now */
+}
+
+/* Restore before terminal i/o settings */
+void resetTermios(void)
+{
+	tcsetattr(0, TCSANOW, &before);
+}
+
+/* Read 1 character - echo defines echo mode */
+unsigned char getch_(int echo)
+{
+	unsigned char ch;
+	initTermios(echo);
+	ch = getchar();
+	resetTermios();
+	return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+	return getch_(0);
+}
+
+/* Read 1 character with echo */
+unsigned char getche(void)
+{
+	return getch_(1);
+}
+
+int sameCheck(unsigned char *num_arr, int index, int repeat){
+	for(int i = 0; i<repeat; i++){
+		if(i==index) continue;
+		
+		if(num_arr[i] == num_arr[index]) return 1;
+	}
+
+	return 0;
+}
+
+int setNum(unsigned char *num_arr, int size) {
+	if (size != 4) return 1;
+
+	int status = 1;
+	unsigned char temp;
+	int index = 3;
+
+	while (status) {
+		while (temp != ENTER) {
+			temp = getche();
+			switch (temp) {
+			case LEFT:
+				if (index < 3) index++;
+				break;
+			case RIGHT:
+				if (index > 0) index--;
+				break;
+			case ZERO:
+			case KEY_ZERO:
+				num_arr[index] = '0';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;		
+				break;
+			case ONE:
+			case KEY_ONE:
+				num_arr[index] = '1';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case TWO:
+			case KEY_TWO:
+				num_arr[index] = '2';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case THREE:
+			case KEY_THREE:
+				num_arr[index] = '3';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case FOUR:
+			case KEY_FOUR:
+				num_arr[index] = '4';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case FIVE:
+			case KEY_FIVE:
+				num_arr[index] = '5';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case SIX:
+			case KEY_SIX:
+				num_arr[index] = '6';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case SEVEN:
+			case KEY_SEVEN:
+				num_arr[index] = '7';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case EIGHT:
+			case KEY_EIGHT:
+				num_arr[index] = '8';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case NINE:
+			case KEY_NINE:
+				num_arr[index] = '9';
+				if (sameCheck(num_arr, index, size)) num_arr[index] = 0;
+				else if (index > 0) index--;	
+				break;
+			case BACK_SPACE:
+				num_arr[index] = 0;
+				if (index < 3) index++;
+				break;
+			default:
+
+			}
+
+
+		}
+		if (!num_arr[3]) {
+			index = 3;
+		}
+		else if (!num_arr[2]) {
+			index = 2;
+		}
+		else if (!num_arr[1]) {
+			index = 1;
+		}
+		else if (!num_arr[0]) {
+			index = 0;
+		}
+		else {
+			status = 0;
+		}
+	}
+
+	return 0;
+}
+
+void checkResult(unsigned char *target_arr, unsigned char *answer_arr, int *strike, int *ball){
+	*strike = 0;
+	*ball = 0;
+
+	for(int i = 1; i < 4; i++){
+		for(int j = 1; j < 4; j++){
+			if(target_arr[i] == answer_arr[j]){
+				if(i == j) (*strike)++;
+				else (*ball)++;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int showResult(int *strike, int *ball){
+	if(*strike == 4) return 0;
+}
+
+int fndUpdate(int dev, char *data) {
+	ret = write(dev, data, FND_MAX_DIGIT);
+	assert2(ret >= 0, "Device write error", FND_DEVICE);
+	sleep(1);
+
+	memset(data, 0, sizeof(data));
+	ret = read(dev, data, FND_MAX_DIGIT);
+	assert2(ret >= 0, "Device read error", FND_DEVICE);
+
+	printf("Current FND value: ");
+	for (i = 0; i < data_len; i++) {
+		printf("%d", data[i]);
+	}
+	printf("\n");
+}
+
+/* Let's test it out */
+int main(void) {
+	int fnd_dev;
+
+	fnd_dev = open(FND_DEVICE, O_RDWR);
+	assert2(fnd_dev >= 0, "Device open error", FND_DEVICE);
+
+	unsigned char target_num[4];
+	unsigned char answer_num[4];
+
+	memset(target_num, 0, sizeof(target_num));
+	memset(answer_num, 0, sizeof(answer_num));
+
+	unsigned char temp;
+	int result = 1;
+	int strike, ball;
+
+	if (setNum(target_num, sizeof(target_num))) return 1;
+
+	while(result){
+		if (setNum(answer_num, sizeof(answer_num))) return 1;
+		checkResult(target_num, answer_num, &strike, &ball);
+		result = showResult(&strike, &ball);
+	}
+
+
+
+
+	close(fnd_dev);
+	printf("Program end.\n");
+
+	return 0;
+}
