3604d4fa5b28338921861b4ccdf42c957f688ad3
Daeseong Yu <yu5501@nate.com>
2019-05-31 15:27:01 +0900

    키 입력

--- Linux/the_game/dot/fpga_dot_test.c
+++ Linux/the_game/dot/fpga_dot_test.c
@@ -12,47 +12,14 @@ int main(int argc, char **argv) {
 	dev = open(DOT_DEVICE, O_WRONLY);
 	assert2(dev >= 0, "Device open error", DOT_DEVICE);
 
+	while () {
+		printf("insert num : ");
+		scanf("%d", &num);
 
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
-	ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
-	assert2(ret >= 0, "Device write error", DOT_DEVICE);
-	sleep(1000);
-	num++;
-
+		ret = write(dev, fpga_number[num], sizeof(fpga_number[num]));
+		assert2(ret >= 0, "Device write error", DOT_DEVICE);
+		sleep(1);
+	}
 
 	close(dev);
 	return 0;
