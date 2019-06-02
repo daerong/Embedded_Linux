5aa8fa1f2a9b3d4edcd72916d98f9ef9b2bdba26
Daeseong Yu <yu5501@nate.com>
2019-06-02 15:05:34 +0900

    sh 파일 작성

---
+++ Linux/include/off.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rmmod fpga_fnd_driver.ko
+rmmod fpga_led_driver.ko
+rmmod fpga_push_switch_driver.ko
+rmmod fpga_dot_driver.ko
+rmmod fpga_text_lcd_driver.ko
+rmmod fpga_step_motor_driver.ko
+rmmod fpga_buzzer_driver.ko
+rmmod fpga_dip_switch_driver.ko
