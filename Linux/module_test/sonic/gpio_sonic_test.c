c1d0227dc9bdebc17cc11d7b923b77ff4113cbf4
Daeseong Yu <yu5501@nate.com>
2019-06-02 15:39:49 +0900

    sonic 테스트 소스

---
+++ Linux/module_test/sonic/gpio_sonic_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+int main(void){
+	int fd;
+	int retn;
+	char buf[10] = { 0 };
+	int loop = 0;
+	fd = open("/dev/us", O_RDWR);
+	printf("fd = %d\n", fd);
+	if (fd < 0) {
+		perror("/dev/us error");
+		exit(-1);
+	}
+	else {
+		printf("< us device has been detected >\n");
+	}
+	while (1) {
+		read(fd, buf, 2);
+		for (loop = 0; loop < 100000; loop++) {};
+	}
+	close(fd);
+	return 0;
+}
