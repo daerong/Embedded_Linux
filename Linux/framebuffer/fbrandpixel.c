c80af6f3ce4300c9c28a58b04dcbca9ca96fd41f
Daeseong Yu <yu5501@nate.com>
2019-06-05 14:17:55 +0900

    no message

--- Linux/framebuffer/fbrandpixel.c
+++ Linux/framebuffer/fbrandpixel.c
@@ -45,7 +45,7 @@ U16 random_pixel(void)
 }
 
 void put_pixel(struct fb_var_screeninfo *fvs, int fd, int xpos, int ypos, unsigned short pixel) {
-	int offset = ypos * fvs.xres * sizeof(pixel) + xpos * sizeof(pixel);	// (xpos, ypos) 위치
+	int offset = ypos * fvs->xres * sizeof(pixel) + xpos * sizeof(pixel);	// (xpos, ypos) 위치
 	assert(lseek(fd, offset, SEEK_SET) >= 0, "LSeek Error.\n");
-	write(fd, &pixel, fvs.bits_per_pixel / (sizeof(pixel)));			// write 2Byte(16bit)
+	write(fd, &pixel, fv->bits_per_pixel / (sizeof(pixel)));			// write 2Byte(16bit)
 }
