8c20eba73896e42a7d2918683c45db9a73ba8d48
Daeseong Yu <yu5501@nate.com>
2019-06-07 20:22:57 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -6,19 +6,13 @@ typedef int S32;
 
 #define SCREEN_X_MAX 1024
 #define SCREEN_Y_MAX 600
-#define PALETTE_X_END 900
-#define TOOLBAR_X_START 901
-#define TOOLBAR_X_END 1024
+#define TOOLBAR_START 900
 
 typedef struct DISPLAY {
 	int xpos;
 	int ypos;
 	U16 color;
 } DISPLAY;
-typedef struct LOCATE {
-	int xpos;
-	int ypos;
-} LOCATE;
 typedef struct MOUSE_CURSOR {
 	int x;
 	int y;
@@ -28,7 +22,6 @@ U16 makepixel(U32  r, U32 g, U32 b);
 void put_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel);
 void set_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, int xpos, int ypos, unsigned short pixel);
 void reset_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, unsigned short pixel);
-void fill_box(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, LOCATE start, LOCATE end, unsigned short pixel);
 void draw_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target);
 void draw_cursor(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel);
 
@@ -38,29 +31,18 @@ int main(int argc, char** argv) {
 	int mouse_fd;
 	U16 foreground_color;			// U16은 short 즉, 16비트.
 	U16 background_color;			// U16은 short 즉, 16비트.
-	U16 menubox_color;
 	struct fb_var_screeninfo fvs;
 	unsigned short *pfbdata;
 	struct input_event ev;
-	LOCATE start;
-	LOCATE end;
-	start.xpos = TOOLBAR_X_START;
-	start.ypos = 0;
-	end.xpos = TOOLBAR_X_END;
-	end.ypos = SCREEN_Y_MAX;
 
 	MOUSE_CURSOR cur;
 	char draw_mode = 0;
 	DISPLAY display[SCREEN_X_MAX * SCREEN_Y_MAX];
 	foreground_color = makepixel(255, 255, 255);							// white color
 	background_color = makepixel(0, 0, 0);									// black color
-	menubox_color = makepixel(50, 150, 150);
 	reset_display(&fvs, pfbdata, display, background_color);
-	fill_box(&fvs, pfbdata, display, start, end, menubox_color);
-	draw_display(&fvs, pfbdata, display);
 
-
-	cur.x = PALETTE_X_END / 2;
+	cur.x = SCREEN_X_MAX / 2;
 	cur.y = SCREEN_Y_MAX / 2;
 	int past_x = cur.x;
 	int past_y = cur.y;
@@ -135,7 +117,7 @@ int main(int argc, char** argv) {
 
 		if (draw_mode) {
 			set_pixel(&fvs, pfbdata, display, cur.x, cur.y, foreground_color);
-			if (cur.x < PALETTE_X_END) put_pixel(&fvs, pfbdata, cur.x, cur.y, foreground_color);
+			put_pixel(&fvs, pfbdata, cur.x, cur.y, foreground_color);
 		}
 		else {
 			draw_cursor(&fvs, pfbdata, past_x, past_y, background_color);
@@ -162,7 +144,6 @@ U16 makepixel(U32  r, U32 g, U32 b) {
 }
 
 void put_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel) {
-	if (xpos >= PALETTE_X_END) return;
 	int offset = ypos * fvs->xres + xpos;
 	pfbdata[offset] = pixel;
 }
@@ -175,32 +156,8 @@ void reset_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPL
 	int x_temp, y_temp;
 
 	for (y_temp = 0; y_temp < SCREEN_Y_MAX; y_temp++) {
-		for (x_temp = 0; x_temp < PALETTE_X_END; x_temp++) {
+		for (x_temp = 0; x_temp < SCREEN_X_MAX; x_temp++) {
 			set_pixel(fvs, pfbdata, target, x_temp, y_temp, pixel);
-		}
-	}
-}
-
-void fill_box(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, LOCATE start, LOCATE end, unsigned short pixel) {
-	int x_start, y_start, x_end, y_end;
-	int x_temp, y_temp;
-
-	if (start.xpos < 0) x_start = 0;
-	else x_start = start.xpos;
-
-	if (start.ypos < 0) y_start = 0;
-	else y_start = start.ypos;
-
-	if (end.xpos > SCREEN_X_MAX - 1) x_end = SCREEN_X_MAX - 1;
-	else x_end = end.xpos;
-
-	if (end.ypos> SCREEN_Y_MAX - 1) y_end = SCREEN_Y_MAX - 1;
-	else y_end = end.ypos;
-
-	for (y_temp = y_start; y_temp < y_end; y_temp++) {
-		for (x_temp = x_start; x_temp < x_end; x_temp++) {
-			set_pixel(fvs, pfbdata, target, x_temp, y_temp, pixel);
-			//target[y_temp*SCREEN_X_MAX + x_temp].color = pixel;
 		}
 	}
 }
