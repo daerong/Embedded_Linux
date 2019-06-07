3fc139a5cf6f793a105caf5c23788e366ede9aee
Daeseong Yu <yu5501@nate.com>
2019-06-07 15:33:31 +0900

    no message

--- Linux/mouse/mouse_app.c
+++ Linux/mouse/mouse_app.c
@@ -27,7 +27,11 @@ int main(int argc, char** argv) {
 			break;
 		}
 
-		printf("type : %hu, code : %hu, value : %d\n", ev.type, ev.code, ev.value);
+		if (ev.value == 1) {
+			if (ev.type == 1) {
+				printf("type : %hu, code : %hu, value : %d\n", ev.type, ev.code, ev.value);
+			}
+		}
 	}
 
 	close(fd);
