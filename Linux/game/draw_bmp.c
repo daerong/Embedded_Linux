e06bdb52906ff7b7e0cf0185151f462dc657af0c
Daeseong Yu <yu5501@nate.com>
2019-06-09 17:26:38 +0900

    no message

--- Linux/game/draw_bmp.c
+++ Linux/game/draw_bmp.c
@@ -1,71 +1,35 @@
-#include "../include/fpga_test.h"
+#include "../include/fpga_frame_buffer.h"
+
+/*
+3번 예제 : 16 bpp 점(pixel) 찍기, write
+
+각 픽셀은 각각의 RGB 값을 가진다. | R(5bit) | G(6bit) | B(5bit) | Red와 Blue는 각각 5비트씩이고 G는 6비트 정보를 가지게 됩니다.
+그리고 MSB(Most Significant Bit)에서 LSB(Least Significant Bit)쪽으로 R, G, B 순으로 저장됩니다.
+
+16bpp가 아니면 실행이 되지 않도록 체크 코드가 들어 있습니다.
+
+한 가지 기억해 둘 것은 한 개의 점을 원하는 위치에 찍기 위해서 두 개의 시스템 호출(lseek, write)을 해야 한다는 것입니다.
+이것이 원초적이고 무식한(?) 이유입니다. 나중에 배울 새로운 점 찍기 루틴을 보고, 성능 비교를 해보면 왜 무식한지 알 수 있을 겁니다.
+*/
 
 typedef unsigned int U32;
 typedef short U16;
-typedef int S32;
 
-#define SCREEN_X_MAX 1024
-#define SCREEN_Y_MAX 600
-#define PALETTE_X_END 900
-#define TOOLBAR_X_START 901
-#define TOOLBAR_X_END 1024
+U16 makepixel(U32  r, U32 g, U32 b) {
+	U16 x = (U16)(r >> 3);
+	U16 y = (U16)(g >> 2);
+	U16 z = (U16)(b >> 3);
 
-typedef struct DISPLAY {
-	int xpos;
-	int ypos;
-	U16 color;
-} DISPLAY;
-typedef struct LOCATE {
-	int xpos;
-	int ypos;
-} LOCATE;
-typedef struct MOUSE_CURSOR {
-	int x;
-	int y;
-} MOUSE_CURSOR;
+	return (z | (x << 11) | (y << 5));
+}
 
-U16 makepixel(U32  r, U32 g, U32 b);
-void put_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel);
-void set_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, int xpos, int ypos, unsigned short pixel);
-void reset_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, unsigned short pixel);
-void fill_box(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, LOCATE start, LOCATE end, unsigned short pixel);
-void draw_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target);
-void draw_cursor(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel);
-void erase_cursor(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel);
-
-U16 menubox_color;
+void put_pixel(struct fb_var_screeninfo *fvs, int fd, int xpos, int ypos, unsigned short pixel);
 
 int main(int argc, char** argv) {
 	int ret;
 	int frame_fd;
-	int mouse_fd;
-	U16 foreground_color;
-	U16 background_color;
+	U16 pixel;			// U16은 short 즉, 16비트. 
 	struct fb_var_screeninfo fvs;
-	unsigned short *pfbdata;
-	struct input_event ev;
-
-	MOUSE_CURSOR cur;
-	char draw_mode = 0;
-	DISPLAY display[SCREEN_X_MAX * SCREEN_Y_MAX];
-
-
-	LOCATE start;
-	LOCATE end;
-	start.xpos = TOOLBAR_X_START;
-	start.ypos = 0;
-	end.xpos = TOOLBAR_X_END;
-	end.ypos = SCREEN_Y_MAX;
-
-	cur.x = SCREEN_X_MAX / 2;
-	cur.y = SCREEN_Y_MAX / 2;
-	int past_x = cur.x;
-	int past_y = cur.y;
-
-	foreground_color = makepixel(255, 255, 255);							// white color
-	background_color = makepixel(0, 0, 0);									// black color
-	menubox_color = makepixel(50, 150, 150);
-
 
 	frame_fd = open(LCD_DEVICE, O_RDWR);
 	assert2(frame_fd >= 0, "Frame Buffer Open Error!", LCD_DEVICE);
@@ -76,18 +40,13 @@ int main(int argc, char** argv) {
 	assert(fvs.bits_per_pixel == 16, "bpp is not 16\n");			// bpp check
 	assert(lseek(frame_fd, 0, SEEK_SET) >= 0, "LSeek Error.\n");	// lseek error check
 
-	pfbdata = (unsigned short *)mmap(0, fvs.xres*fvs.yres * sizeof(U16), PROT_READ | PROT_WRITE, MAP_SHARED, frame_fd, 0);
-	assert((unsigned)pfbdata != (unsigned)-1, "fbdev mmap error.\n");
-
-
-
-
 
 	FILE *fp;
 	unsigned char info[54];
 
 	fp = fopen("lenna.bmp", "rb");
-	if (fp == NULL){
+	if (fp == NULL) {
+		printf("File open error: ");
 		perror("File open error: ");
 		exit(0);
 	}
@@ -104,12 +63,11 @@ int main(int argc, char** argv) {
 	fread(data, sizeof(unsigned char), size, fp);
 
 	int i;
-	U16 pixel_color;
 	int vertical = 0;
 	int horizon = 0;
 	for (i = 0; i < size; i += 3) {
-		pixel_color = makepixel(data[i + 2], data[i], data[i + 1]);
-		set_pixel(&fvs, pfbdata, display, horizon, vertical, pixel_color);
+		pixel = makepixel(data[i + 2], data[i], data[i + 1]);
+		put_pixel(&fvs, frame_fd, horizon, width, pixel);
 		if (horizon < width) horizon++;
 		else {
 			horizon = 0;
@@ -117,119 +75,16 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	reset_display(&fvs, pfbdata, display, background_color);
-	draw_display(&fvs, pfbdata, display);
 
-	munmap(pfbdata, fvs.xres*fvs.yres * sizeof(U16));
+	printf("%d\n", sizeof(pixel));
 	close(frame_fd);
 	fclose(fp);
 
 	return 0;
 }
 
-U16 makepixel(U32  r, U32 g, U32 b) {
-	U16 x = (U16)(r >> 3);
-	U16 y = (U16)(g >> 2);
-	U16 z = (U16)(b >> 3);
-
-	return (z | (x << 11) | (y << 5));
-}
-
-void put_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel) {
-	int offset = ypos * fvs->xres + xpos;
-	pfbdata[offset] = pixel;
-}
-
-void set_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, int xpos, int ypos, unsigned short pixel) {
-	target[ypos*SCREEN_X_MAX + xpos].color = pixel;
-}
-
-void reset_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, unsigned short pixel) {
-	int x_temp, y_temp;
-
-	for (y_temp = 0; y_temp < SCREEN_Y_MAX; y_temp++) {
-		for (x_temp = 0; x_temp < PALETTE_X_END; x_temp++) {
-			set_pixel(fvs, pfbdata, target, x_temp, y_temp, pixel);
-		}
-	}
-}
-
-void fill_box(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, LOCATE start, LOCATE end, unsigned short pixel) {
-	int x_start, y_start, x_end, y_end;
-	int x_temp, y_temp;
-
-	if (start.xpos < 0) x_start = 0;
-	else x_start = start.xpos;
-
-	if (start.ypos < 0) y_start = 0;
-	else y_start = start.ypos;
-
-	if (end.xpos > SCREEN_X_MAX - 1) x_end = SCREEN_X_MAX - 1;
-	else x_end = end.xpos;
-
-	if (end.ypos > SCREEN_Y_MAX - 1) y_end = SCREEN_Y_MAX - 1;
-	else y_end = end.ypos;
-
-	for (y_temp = y_start; y_temp < y_end; y_temp++) {
-		for (x_temp = x_start; x_temp < x_end; x_temp++) {
-			set_pixel(fvs, pfbdata, target, x_temp, y_temp, pixel);
-			//target[y_temp*SCREEN_X_MAX + x_temp].color = pixel;
-		}
-	}
-}
-
-void draw_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target) {
-	int x_temp, y_temp;
-
-	for (y_temp = 0; y_temp < SCREEN_Y_MAX; y_temp++) {
-		for (x_temp = 0; x_temp < SCREEN_X_MAX; x_temp++) {
-			put_pixel(fvs, pfbdata, x_temp, y_temp, target[y_temp*SCREEN_X_MAX + x_temp].color);
-		}
-	}
-}
-
-void erase_cursor(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel) {
-	int i, j;
-	for (i = 0; i < 10; i++) {
-		for (j = 0; j < 10; j++) {
-			if (j + i <= 10) {
-				if (ypos + i > SCREEN_Y_MAX - 1 || xpos + j > SCREEN_X_MAX - 1) continue;
-				int offset = (ypos + i) * fvs->xres + (xpos + j);
-				if (xpos + j < PALETTE_X_END) pfbdata[offset] = pixel;
-				else pfbdata[offset] = menubox_color;
-			}
-		}
-	}
-	for (i = 5; i < 15; i++) {
-		for (j = i; j < i + 3; j++) {
-			{
-				if (ypos + i > SCREEN_Y_MAX - 1 || xpos + j > SCREEN_X_MAX - 1) continue;
-				int offset = (ypos + i) * fvs->xres + (xpos + j);
-				if (xpos + j < PALETTE_X_END) pfbdata[offset] = pixel;
-				else pfbdata[offset] = menubox_color;
-			}
-		}
-	}
-}
-
-void draw_cursor(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel) {
-	int i, j;
-	for (i = 0; i < 10; i++) {
-		for (j = 0; j < 10; j++) {
-			if (j + i <= 10) {
-				if (ypos + i > SCREEN_Y_MAX - 1 || xpos + j > SCREEN_X_MAX - 1) continue;
-				int offset = (ypos + i) * fvs->xres + (xpos + j);
-				pfbdata[offset] = pixel;
-			}
-		}
-	}
-	for (i = 5; i < 15; i++) {
-		for (j = i; j < i + 3; j++) {
-			{
-				if (ypos + i > SCREEN_Y_MAX - 1 || xpos + j > SCREEN_X_MAX - 1) continue;
-				int offset = (ypos + i) * fvs->xres + (xpos + j);
-				pfbdata[offset] = pixel;
-			}
-		}
-	}
+void put_pixel(struct fb_var_screeninfo *fvs, int fd, int xpos, int ypos, unsigned short pixel) {
+	int offset = ypos * fvs->xres * sizeof(pixel) + xpos * sizeof(pixel);	// (xpos, ypos) 위치
+	assert(lseek(fd, offset, SEEK_SET) >= 0, "LSeek Error.\n");
+	write(fd, &pixel, fvs->bits_per_pixel / (sizeof(pixel)));			// write 2Byte(16bit)
 }
