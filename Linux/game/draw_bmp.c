b5b16743ba8da17fa25a24be40cca9ffb2806884
Daeseong Yu <yu5501@nate.com>
2019-06-09 17:32:22 +0900

    no message

--- Linux/game/draw_bmp.c
+++ Linux/game/draw_bmp.c
@@ -50,18 +50,18 @@ int main(int argc, char** argv) {
 
 	fread(data, sizeof(unsigned char), size, fp);
 
-	int i;
 	int vertical = 0;
 	int horizon = 0;
-	for (i = 0; i < size; i += 3) {
-		pixel = makepixel(data[i + 2], data[i], data[i + 1]);
-		put_pixel(&fvs, frame_fd, horizon, width, pixel);
-		if (horizon < width) horizon++;
-		else {
-			horizon = 0;
-			vertical++;
+	int locate = 0;
+
+	for (vertical = 0; vertical < height; vertical++) {
+		for (horizon = 0; horizon < width; horizon++) {
+			locate = vertical * width + horizon;
+			pixel = makepixel(data[locate + 2], data[locate], data[locate + 1]);
+			put_pixel(&fvs, frame_fd, horizon, width, pixel);
 		}
 	}
+
 
 
 	printf("%d\n", sizeof(pixel));
