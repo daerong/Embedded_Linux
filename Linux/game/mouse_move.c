1221110ebffc8010580a12de493a825a14544c50
Daeseong Yu <yu5501@nate.com>
2019-06-09 21:49:28 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -35,7 +35,7 @@ typedef struct MOUSE_CURSOR {
 U16 makepixel(U32  r, U32 g, U32 b);
 void put_pixel(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel);
 void set_pixel(DISPLAY *target, int xpos, int ypos, unsigned short pixel);
-void reset_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, unsigned short pixel);
+void reset_display(DISPLAY *target, DISPLAY *background);
 void fill_box(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, LOCATE start, LOCATE end, unsigned short pixel);
 void draw_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target);
 void draw_cursor(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, int xpos, int ypos, unsigned short pixel);
@@ -100,14 +100,8 @@ void set_pixel(DISPLAY *target, int xpos, int ypos, unsigned short pixel) {
 	target[ypos*SCREEN_X_MAX + xpos].color = pixel;
 }
 
-void reset_display(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, unsigned short pixel) {
-	int x_temp, y_temp;
-
-	for (y_temp = 0; y_temp < SCREEN_Y_MAX; y_temp++) {
-		for (x_temp = 0; x_temp < PALETTE_X_END; x_temp++) {
-			set_pixel(target, x_temp, y_temp, pixel);
-		}
-	}
+void reset_display(DISPLAY *target, DISPLAY *background) {
+	memcpy(target, background, sizeof(DISPLAY)*SCREEN_X_MAX*SCREEN_Y_MAX);
 }
 
 void fill_box(struct fb_var_screeninfo *fvs, unsigned short *pfbdata, DISPLAY *target, LOCATE start, LOCATE end, unsigned short pixel) {
@@ -279,12 +273,9 @@ void* mouse_ev_func(void *data) {
 	pfbdata = (unsigned short *)mmap(0, fvs.xres*fvs.yres * sizeof(U16), PROT_READ | PROT_WRITE, MAP_SHARED, frame_fd, 0);
 	assert((unsigned)pfbdata != (unsigned)-1, "fbdev mmap error.\n");
 
-	reset_display(&fvs, pfbdata, background, background_color);
 	fill_box(&fvs, pfbdata, background, start, end, menubox_color);
 	set_image(&fvs, pfbdata, background, 0, 0, "lenna.bmp");
-
-	memcpy(display, background, sizeof(DISPLAY)*SCREEN_X_MAX*SCREEN_Y_MAX);
-	
+	reset_display(display, background);
 	draw_display(&fvs, pfbdata, display);
 
 	while (1) {
@@ -312,7 +303,7 @@ void* mouse_ev_func(void *data) {
 					}
 				}
 				else if (ev.code == 273) {
-					reset_display(&fvs, pfbdata, display, background_color);
+					reset_display(display, background);
 					draw_display(&fvs, pfbdata, display);
 				}
 			}
