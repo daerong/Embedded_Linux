b23b0ebba99fbba54b5032fce0009684896c201b
Daeseong Yu <yu5501@nate.com>
2019-06-10 13:35:05 +0900

    sh 파일 세팅

---
+++ Linux/images/icon.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+tftp -r background.bmp -g 192.168.1.70
+tftp -r icon_1.bmp -g 192.168.1.70
+tftp -r icon_1_on.bmp -g 192.168.1.70
+tftp -r icon_2.bmp -g 192.168.1.70
+tftp -r icon_2_on.bmp -g 192.168.1.70
+tftp -r icon_3.bmp -g 192.168.1.70
+tftp -r icon_3_on.bmp -g 192.168.1.70
+tftp -r icon_4.bmp -g 192.168.1.70
+tftp -r icon_4_on.bmp -g 192.168.1.70
+tftp -r icon_defalut.bmp -g 192.168.1.70
+tftp -r lenna.bmp -g 192.168.1.70
