4b9a05819f5b50d893d39aec3ed5ce049c34c359
Daeseong Yu <yu5501@nate.com>
2019-06-10 16:22:50 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -334,8 +334,66 @@ void* mouse_ev_func(void *data) {
 			if (ev.value == 1) {
 				if (ev.code == 272) {
 					if (cur.x > TOOLBAR_X_START) {
-						if (text_lcd_mode) text_lcd_mode = 0;
-						else text_lcd_mode = 1;
+						if (cur.x >= ICON_START && cur.x < ICON_END) {
+							if (cur.y >= ICON_1_Y_START && cur.y < ICON_1_Y_START + ICON_WIDTH) {	// 채팅
+								if (text_lcd_mode) {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_1_Y_START, "icon_1.bmp");
+									text_lcd_mode = 0;
+								}
+								else {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_1_Y_START, "icon_1_on.bmp");
+									text_lcd_mode = 1;
+								}
+							}
+							else if (cur.y >= ICON_2_Y_START && cur.y < ICON_2_Y_START + ICON_WIDTH) {	// 카메라
+								if (camera_mode) {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_2_Y_START, "icon_2.bmp");
+									camera_mode = 0;
+								}
+								else {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_2_Y_START, "icon_2_on.bmp");
+									camera_mode = 1;
+								}
+							}
+							else if (cur.y >= ICON_3_Y_START && cur.y < ICON_3_Y_START + ICON_WIDTH) {	// 숫자야구
+								if (num_baseball_mode) {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_3_Y_START, "icon_3.bmp");
+									num_baseball_mode = 0;
+								}
+								else {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_3_Y_START, "icon_3_on.bmp");
+									num_baseball_mode = 1;
+								}
+							}
+							else if (cur.y >= ICON_4_Y_START && cur.y < ICON_4_Y_START + ICON_WIDTH) {	// lenna image
+								if (lenna_img_mode) {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_4_Y_START, "icon_4.bmp");
+									lenna_img_mode = 0;
+								}
+								else {
+									set_image(&fvs, pfbdata, background, ICON_START, ICON_4_Y_START, "icon_4_on.bmp");
+									lenna_img_mode = 1;
+								}
+							}
+							else if (cur.y >= ICON_5_Y_START && cur.y < ICON_5_Y_START + ICON_WIDTH) {
+
+							}
+							else if (cur.y >= ICON_6_Y_START && cur.y < ICON_6_Y_START + ICON_WIDTH) {
+
+							}
+							else if (cur.y >= ICON_7_Y_START && cur.y < ICON_7_Y_START + ICON_WIDTH) {
+
+							}
+							else if (cur.y >= ICON_8_Y_START && cur.y < ICON_8_Y_START + ICON_WIDTH) {
+
+							}
+							else if (cur.y >= ICON_9_Y_START && cur.y < ICON_9_Y_START + ICON_WIDTH) {
+
+							}
+							else if (cur.y >= ICON_10_Y_START && cur.y < ICON_10_Y_START + ICON_WIDTH) {
+
+							}
+						}
 					}
 					else {
 						if (draw_mode) draw_mode = 0;
