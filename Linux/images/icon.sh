233112fb447037a6f69cc1604cae73d49e2439de
Daeseong Yu <yu5501@nate.com>
2019-06-10 17:51:07 +0900

    no message

--- Linux/images/icon.sh
+++ Linux/images/icon.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 tftp -r background.bmp -g 192.168.1.70
-tftp -r icon_1.bmp -g 192.168.1.70
-tftp -r icon_1_on.bmp -g 192.168.1.70
-tftp -r icon_2.bmp -g 192.168.1.70
-tftp -r icon_2_on.bmp -g 192.168.1.70
-tftp -r icon_3.bmp -g 192.168.1.70
-tftp -r icon_3_on.bmp -g 192.168.1.70
-tftp -r icon_4.bmp -g 192.168.1.70
-tftp -r icon_4_on.bmp -g 192.168.1.70
-tftp -r icon_defalut.bmp -g 192.168.1.70
+tftp -r icon1.bmp -g 192.168.1.70
+tftp -r icon1on.bmp -g 192.168.1.70
+tftp -r icon2.bmp -g 192.168.1.70
+tftp -r icon2on.bmp -g 192.168.1.70
+tftp -r icon3.bmp -g 192.168.1.70
+tftp -r icon3on.bmp -g 192.168.1.70
+tftp -r icon4.bmp -g 192.168.1.70
+tftp -r icon4on.bmp -g 192.168.1.70
+tftp -r icondefalut.bmp -g 192.168.1.70
 tftp -r lenna.bmp -g 192.168.1.70
