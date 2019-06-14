a8671d23222d2ae8f44f2dd8ce35744fb1098fdb
Daeseong Yu <yu5501@nate.com>
2019-06-14 14:43:41 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -555,6 +555,7 @@ void* mouse_ev_func(void *data) {
 			set_small_image(&fvs, pfbdata, proc_display, ICON_START, ICON_1_Y_START, "icon1.bmp");
 			menu_copy(display, proc_display);
 			menu_update(&fvs, pfbdata, display);
+			icon_off = 0;
 			break;
 		}
 
