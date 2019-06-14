569b9e6d44f5676348e8e2bd2874f936fcf45d0b
Daeseong Yu <yu5501@nate.com>
2019-06-14 19:05:41 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -869,7 +869,7 @@ void* send_msg(void* arg){
 
 	while (1){
 		if (send_msg_stat) {
-			fgets(msg, MSG_BUF_SIZE, stdin);
+			memset(msg, ' ', MSG_BUF_SIZE);
 			memcpy(msg, text_lcd_buf, TEXT_LCD_LINE_BUF);
 			sprintf(name_msg, "%s %s", name, msg);
 			write(sock, name_msg, strlen(name_msg));
