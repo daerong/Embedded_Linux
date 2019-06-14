8ef44e2a45ba9891051684425a28422b62f1a2eb
Daeseong Yu <yu5501@nate.com>
2019-06-14 20:12:32 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -900,7 +900,8 @@ void* recv_msg(void* arg)
 		if (str_len == -1)
 			return (void*)-1;
 		name_msg[str_len] = 0;
-		memcpy(text_lcd_buf, msg, TEXT_LCD_LINE_BUF);
+		printf("%s", name_msg);
+		//memcpy(text_lcd_buf, msg, TEXT_LCD_LINE_BUF);
 		recv_msg_stat = 1;
 		fputs(name_msg, stdout);
 	}
