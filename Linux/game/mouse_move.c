8a727f65adfef0c5133afeaa0f599cc012356ed0
Daeseong Yu <yu5501@nate.com>
2019-06-14 21:12:23 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -893,32 +893,16 @@ void* recv_msg(void* arg)
 {
 	int sock = *((int*)arg);
 	char name_msg[NORMAL_SIZE + MSG_BUF_SIZE];
-	char clean_msg[NORMAL_SIZE + MSG_BUF_SIZE];
 	int str_len;
-	int show_len;
-	char *recv_trash;
-	char *recv_chat;
 
 	while (1){
 		str_len = read(sock, name_msg, NORMAL_SIZE + MSG_BUF_SIZE - 1);
-		if (str_len == -1) {
+		if (str_len == -1)
 			return (void*)-1;
-		}
-		else {
-			memset(clean_msg, ' ', NORMAL_SIZE + MSG_BUF_SIZE);
-			memcpy(clean_msg, name_msg, str_len);
-			printf("%d\n", str_len);
-		}
-
 		name_msg[str_len] = 0;
 		fputs(name_msg, stdout);
 
-		char *ptr = strtok(clean_msg, "]");   // 공백 문자열을 기준으로 문자열을 자름
-		recv_trash = ptr;
-		ptr = strtok(NULL, " ");   // 다음 문자열을 잘라서 포인터를 반환
-		recv_chat = ptr;
-
-		strncpy(text_lcd_buf, recv_chat, TEXT_LCD_LINE_BUF);
+		strncpy(text_lcd_buf, name_msg, TEXT_LCD_LINE_BUF);
 		recv_msg_stat = 1;
 	}
 	return NULL;
