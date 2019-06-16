76f95de36f5ea29ce84e9995b37c0ecdc65b7761
Daeseong Yu <yu5501@nate.com>
2019-06-16 20:21:57 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -128,7 +128,7 @@ int main(int argc, char* argv[]) {
 	char usage[50];
 	char buzzer_state = BUZZER_OFF;
 	ssize_t ret;
-	int target;
+	int target = 0;
 
 	unsigned char push_sw_buf[PUSH_SWITCH_MAX_BUTTON];
 	unsigned char target_num[4] = { 7,5,3,1 };
@@ -181,30 +181,30 @@ int main(int argc, char* argv[]) {
 		for (buf_locate = 0; buf_locate < PUSH_SWITCH_MAX_BUTTON; buf_locate++) {
 			if (push_sw_buf[buf_locate] == 1) {
 				answer_num[target] = buf_locate + 1;
-				ret = write(dot_dev, fpga_number[buf_locate + 1], sizeof(fpga_number[buf_locate + 1]));
-				if (target < 4) target++;
-				else target = 0;
 				break;
 			}
 		}
 
+		target++;
+
+		ret = write(dot_dev, fpga_number[buf_locate + 1], sizeof(fpga_number[buf_locate + 1]));
 		ret = write(fnd_dev, answer_num, FND_MAX_DIGIT);
 		assert2(ret >= 0, "Device write error", FND_DEVICE);
 
-
-		if (target_num[3] == answer_num[3]) led_data += 16;
-		if (target_num[2] == answer_num[2]) led_data += 32;
-		if (target_num[1] == answer_num[1]) led_data += 64;
-		if (target_num[0] == answer_num[0]) led_data += 128;
-		if (led_data == 240) status = 0;
+		if (target_num[0] == answer_num[0] && target_num[1] == answer_num[1] && target_num[2] == answer_num[2] && target_num[3] == answer_num[3]) status = 0;
+		//if (target_num[3] == answer_num[3]) led_data += 16;
+		//if (target_num[2] == answer_num[2]) led_data += 32;
+		//if (target_num[1] == answer_num[1]) led_data += 64;
+		//if (target_num[0] == answer_num[0]) led_data += 128;
+		//if (led_data == 240) status = 0;
 
 
 		assert(LEDS_MIN <= led_data && led_data <= LEDS_MAX, "Invalid parameter range");
 
 		ret = write(led_dev, &led_data, 1);
 		assert2(ret >= 0, "Device write error", LEDS_DEVICE);
+		if (target == 4 && status != 0) memset(answer_num, 0, sizeof(answer_num));
 
-		if (answer_num[0] != 0 && answer_num[1] != 0 && answer_num[2] != 0 && answer_num[3] != 0) memset(answer_num, 0, sizeof(answer_num));
 	}
 
 	close(fnd_dev);
