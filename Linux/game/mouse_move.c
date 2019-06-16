23476a537176bfae701e39a544405e085f049a7d
Daeseong Yu <yu5501@nate.com>
2019-06-16 19:10:50 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -804,6 +804,7 @@ void* mouse_ev_func(void *data) {
 									menu_update(&fvs, pfbdata, display);
 									step_motor_update(step_motor_dev, 1, 0, 150);
 									step_motor_mode = 1;
+								}
 							}
 							else if (cur.y >= ICON_6_Y_START && cur.y < ICON_6_Y_START + ICON_WIDTH) {
 
