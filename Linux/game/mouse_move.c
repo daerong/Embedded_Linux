c8d99b0d2228b4a4b120f9454e524b9b55ea3c68
Daeseong Yu <yu5501@nate.com>
2019-06-16 22:40:17 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -748,7 +748,7 @@ void* mouse_ev_func(void *data) {
 			break;
 		}
 
-		if (camera_update) {
+		if (camera_mode) {
 			set_image(&fvs, pfbdata, display, 0, 0, "lenna.bmp");
 			draw_display(&fvs, pfbdata, display);
 			camera_update = 0;
