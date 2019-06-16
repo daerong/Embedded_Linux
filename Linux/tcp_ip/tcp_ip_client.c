0b86179cda8a5279e77e1c8ad2ba21ba6a54587c
Daeseong Yu <yu5501@nate.com>
2019-06-16 10:39:20 +0900

    no message

--- Linux/tcp_ip/tcp_ip_client.c
+++ Linux/tcp_ip/tcp_ip_client.c
@@ -91,7 +91,7 @@ void* send_msg(void* arg)
 		}
 
 		// send message
-		sprintf(name_msg, "%s", name, msg);
+		sprintf(name_msg, "%s", name);
 		write(sock, name_msg, strlen(name_msg));
 	}
 	return NULL;
