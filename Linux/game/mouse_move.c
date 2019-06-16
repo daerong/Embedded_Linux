191a185ee923bd494421f299b87d745c935c8d76
Daeseong Yu <yu5501@nate.com>
2019-06-16 13:36:00 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -886,9 +886,9 @@ void* sonic_func(void *data) {
 		printf("< us device has been detected >\n");
 	}
 	while (1) {
-		read(fd, buf, 4);
-		usleep(200000);
-		printf("distance user : %d (cm)\n", buf);
+		read(fd, buf, 2);
+		for (loop = 0; loop < 1000000; loop++) {};
+		printf("distance user : %d (cm)\n", *buf);
 	}
 	close(fd);
 	free(buf);
