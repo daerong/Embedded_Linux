83e21ca26d84ff0bbbbab16f507baaeaaa323a19
Daeseong Yu <yu5501@nate.com>
2019-06-16 13:10:24 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -872,13 +872,13 @@ void* chat_func(void *data) {
 }
 
 void* sonic_func(void *data) {
-	int fd;
+	int sonic_fd;
 	int retn;
-	int *buf = (int *)malloc(sizeof(int));
+	int buf;
 	int loop = 0;
 	fd = open("/dev/us", O_RDWR);
-	printf("fd = %d\n", fd);
-	if (fd < 0) {
+	printf("fd = %d\n", sonic_fd);
+	if (sonic_fd < 0) {
 		perror("/dev/us error");
 		exit(-1);
 	}
@@ -886,13 +886,14 @@ void* sonic_func(void *data) {
 		printf("< us device has been detected >\n");
 	}
 	while (1) {
-		read(fd, buf, 2);
-		for (loop = 0; loop < 100000; loop++) {};
-		printf("distance user : %d (cm)\n", *buf);
+		read(sonic_fd, buf, 2);
+		usleep(50000);
+		printf("distance user : %d (cm)\n", &buf);
+		usleep(150000);
 	}
 
-	close(fd);
-	free(buf);
+	close(sonic_fd);
+	//free(buf);
 	//pthread_exit((void*)&retval);
 
 	return 0;
