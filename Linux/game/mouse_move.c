fed560475c8ba6cc43725d151cc70456c169bd6c
Daeseong Yu <yu5501@nate.com>
2019-06-16 13:37:22 +0900

    no message

--- Linux/game/mouse_move.c
+++ Linux/game/mouse_move.c
@@ -887,7 +887,7 @@ void* sonic_func(void *data) {
 	}
 	while (1) {
 		read(fd, buf, 2);
-		for (loop = 0; loop < 1000000; loop++) {};
+		usleep(200000);
 		printf("distance user : %d (cm)\n", *buf);
 	}
 	close(fd);
